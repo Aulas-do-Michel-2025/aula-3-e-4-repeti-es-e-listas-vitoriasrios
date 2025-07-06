d17be88fd4a511d4fce01d105d988424eb0a1e41
vitoriasrios <vitoriasrios97@gmail.com>
2025-07-06 17:01:52 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -23,3 +23,12 @@ Os números ímpares são [1, 3, 5]
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+
+
+
+lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
+
+
+impares = [num for num in lista if num % 2 != 0]
+
+print(impares)
